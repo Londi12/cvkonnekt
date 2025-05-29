f12e3c0744af645ad34d6bdc6ff150e5874aabaf
Londi12 <londiwe.shibe@gmail.com>
2025-05-29 09:27:05 +0100

    fix: add Jest and Babel configuration for testing

--- jest.config.js
+++ jest.config.js
@@ -1,31 +1,18 @@
 module.exports = {
   testEnvironment: 'jsdom',
-  setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
+  transform: {
+    '^.+\\.(js|jsx)$': 'babel-jest',
+  },
   moduleNameMapper: {
+    '\\.(css|less|scss|sass)$': 'identity-obj-proxy',
     '^@/(.*)$': '<rootDir>/src/$1',
     '^@components/(.*)$': '<rootDir>/src/components/$1',
-    '^@utils/(.*)$': '<rootDir>/src/utils/$1',
-    '\\.(css|less|scss|sass)$': 'identity-obj-proxy'
+    '^@utils/(.*)$': '<rootDir>/utils/$1'
   },
-  transform: {
-    '^.+\\.(js|jsx|ts|tsx)$': ['babel-jest', { presets: ['@babel/preset-env', '@babel/preset-react'] }]
-  },
-  testMatch: [
-    '<rootDir>/src/**/__tests__/**/*.{js,jsx,ts,tsx}',
-    '<rootDir>/src/**/*.{spec,test}.{js,jsx,ts,tsx}'
+  setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
+  testMatch: ['**/__tests__/**/*.test.[jt]s?(x)'],
+  transformIgnorePatterns: [
+    '/node_modules/(?!(@supabase|@babel)/)'
   ],
-  collectCoverageFrom: [
-    'src/**/*.{js,jsx,ts,tsx}',
-    '!src/**/*.d.ts',
-    '!src/main.jsx',
-    '!src/vite-env.d.ts'
-  ],
-  coverageThreshold: {
-    global: {
-      branches: 80,
-      functions: 80,
-      lines: 80,
-      statements: 80
-    }
-  }
+  moduleFileExtensions: ['js', 'jsx', 'json', 'node']
 }; 
--- jest.setup.js
+++ jest.setup.js
@@ -1,5 +1,15 @@
 import '@testing-library/jest-dom';
 
 // Mock environment variables
-process.env.VITE_SUPABASE_URL = 'https://dbhkdtavxurjiatnymij.supabase.co';
-process.env.VITE_SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImRiaGtkdGF2eHVyamlhdG55bWlqIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDg0Njk3NTQsImV4cCI6MjA2NDA0NTc1NH0.7912j_fXraikvJFQTmg47UfOKejEFs3evG44vflvtLU'; 
+process.env.VITE_SUPABASE_URL = 'https://test.supabase.co';
+process.env.VITE_SUPABASE_ANON_KEY = 'test-anon-key';
+
+// Mock import.meta
+global.import = {
+  meta: {
+    env: {
+      VITE_SUPABASE_URL: 'https://test.supabase.co',
+      VITE_SUPABASE_ANON_KEY: 'test-anon-key'
+    }
+  }
+}; 
